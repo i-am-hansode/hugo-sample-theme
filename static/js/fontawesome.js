077fadb8ca21cd479357db2cf96f77d7dc30fa59
hansode <hansode.log@gmail.com>
2020-05-16 16:33:21 +0900

    webpackとFontAwesomeの導入

---
+++ static/js/fontawesome.js
@@ -0,0 +1,10 @@
+import { config, dom, library } from "@fortawesome/fontawesome-svg-core";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
+const iconArr = [
+  faHome
+]
+
+library.add(iconArr)
+
+dom.watch()
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './static/js/fontawesome.js',
+  output: {
+    path: __dirname + '/assets/js/',
+    filename: 'bundle.js'
+  }
+}
